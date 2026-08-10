0454b88aed947d7fd3647514bae2f4fd7a1572b5
Samarpan Ghosh <samarpanghosh1007@gmail.com>
2026-08-10 12:29:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.64%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProduct(self, n):
+        first=0
+        second=0
+        while n>0:
+            x=n%10
+            if x>first:
+                second=first
+                first=x
+            elif x>second:
+                second=x
+            n=n//10
+        return first*second
+        
+
+        
+       
+
+
+        
