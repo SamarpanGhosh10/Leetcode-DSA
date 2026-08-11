2f748c066ecf48c44e2650a868a3835f3921c7d9
Samarpan Ghosh <samarpanghosh1007@gmail.com>
2026-08-11 09:32:44 +0530

    Time: 4 ms (77.28%), Space: 12.4 MB (93.33%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        
+        result=""
+        count=0
+        for i in s:
+            if i=="(":
+                count+=1
+                if count>1:
+                    result+=i
+            else:
+                count-=1
+                if count>0:
+                    result+=i
+        return result
+
+          
+        
+        
+       
+
+
+        
+        
